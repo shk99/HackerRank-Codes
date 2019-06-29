bb36d4471463571c27b56a740d14ed8c05164e7c
Shoaib Khan <16048878+shk99@users.noreply.github.com>
2019-06-30 02:08:37 +0530

    Create compare-the-triplets.py

---
+++ compare-the-triplets.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import itertools
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    scoreAlice = 0
+    scoreBob = 0
+    array = []
+    bob = []
+
+    for index in range(len(a)):
+        if a[index] > b[index]:
+            scoreAlice += 1  
+        if a[index] == b[index]:
+            pass
+        if a[index] < b[index]:
+            scoreBob += 1
+    array = [scoreAlice,scoreBob]
+    return array 
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
